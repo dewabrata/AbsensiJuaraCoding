17e73fa7bdcde59a7d7f9a96addaeb173401fd70
Dewa <dewabrata@gmail.com>
2019-08-23 14:32:04 +0700

    Create Menu Drawer

--- app/src/main/java/com/juaracoding/absensi/ui/LoginActivity.java
+++ app/src/main/java/com/juaracoding/absensi/ui/LoginActivity.java
@@ -2,15 +2,36 @@ package com.juaracoding.absensi.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.juaracoding.absensi.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextInputEditText txtUsername,txtPassword;
+    FloatingActionButton fabLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        txtUsername = (TextInputEditText)findViewById(R.id.txtUsername);
+        txtPassword = (TextInputEditText)findViewById(R.id.txtPassword);
+        fabLogin = (FloatingActionButton)findViewById(R.id.fabLogin);
+
+
+        fabLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txtUsername.getText().toString().equalsIgnoreCase(txtPassword.getText().toString())){
+
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/juaracoding/absensi/ui/MainActivity.java
+++ app/src/main/java/com/juaracoding/absensi/ui/MainActivity.java
@@ -1,16 +1,65 @@
 package com.juaracoding.absensi.ui;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.juaracoding.absensi.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActionBar actionBar;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_menu);
+        initToolbar();
+        initNavigationMenu();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(getResources().getColor(R.color.pink_600));
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle("Drawer No Icon");
+        //Tools.setSystemBarColor(this, R.color.pink_700);
+    }
+
+    private void initNavigationMenu() {
+        final NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+               // updateCounter(nav_view);
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(final MenuItem item) {
+                Toast.makeText(getApplicationContext(), item.getTitle() + " Selected", Toast.LENGTH_SHORT).show();
+                actionBar.setTitle(item.getTitle());
+                drawer.closeDrawers();
+                return true;
+            }
+        });
+
+        // open drawer at start
+        //drawer.openDrawer(GravityCompat.START);
+       // updateCounter(nav_view);
     }
 }
